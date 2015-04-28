dc642f40cab5b5444af110163c71dc4cfd52992c
Cassio Batista <cassio.batista.13@gmail.com>
2015-04-28 14:07:43 -0300

    cassio: future+metodol+appendix edited

--- tech_report/codes/myanalyze.m
+++ tech_report/codes/myanalyze.m
@@ -4,12 +4,10 @@
 function y = myanalyze(a, b, c, d)
 
 debug = false;
-
 is_vec = [~isvector(a) ~isvector(b) ~isvector(c) ~isvector(d)];
 min_len = [length(a)<35 length(b)<35 length(c)<35 length(d)<35];
-
 if sum([sum(is_vec) sum(min_len)]) ~= 0
-	error('TA ERRADO FILHOS DAS PUAS')
+	error('Error')
 end
 
 % threshold: expecting specify a enough range to get the entire signal
@@ -28,68 +26,42 @@ c = c(2:id);
 [m, id] = max(d(2:myend));
 d = d(2:id);
 
-if debug
-	figure;
-	subplot(4,1,1);
-	plot(a, 'b', 'LineWidth', 3);
-	xlim([0 myend]); set(gca, 'XTick', [0:1:myend]); grid on;
-	subplot(4,1,2);
-	plot(b, 'r', 'LineWidth', 3);
-	xlim([0 myend]); set(gca, 'XTick', [0:1:myend]); grid on;
-	subplot(4,1,3);
-	plot(c, 'g', 'LineWidth', 3);
-	xlim([0 myend]); set(gca, 'XTick', [0:1:myend]); grid on;
-	subplot(4,1,4);
-	plot(d, 'k', 'LineWidth', 3);
-	xlim([0 myend]); set(gca, 'XTick', [0:1:myend]); grid on;
-end
-
-fprintf('dur_a_ms = %.3f \n', sum(a)/1000);
-fprintf('dur_b_ms = %.3f \n', sum(b)/1000);
-fprintf('dur_c_ms = %.3f \n', sum(c)/1000);
-fprintf('dur_d_ms = %.3f \n', sum(d)/1000);
-
-y = mean([sum(a) sum(b) sum(c) sum(d)])/1000;
-
-% Periodo da informação completa (14 bits) RC-5
-% 14 bits * 64 pulsos por bit * periodo de uma piscada do led
-%duration_T_38k = 14 * 2*32*(1/38000) * 1000
-
 % Converting from vector of durations into step function
 % if a piece lasts n usecs then it'll become a step with n ones or zeros
 x_a = x_b = x_c = x_d = [];
 for i=1:length(a)
-	if rem(i, 2) == 1 % odd .:. crista
+	if rem(i, 2) == 1 % odd .:. high peak
 		x_a = [x_a ones(1, a(i))];
-	else
+	else % even .:. low peak
 		x_a = [x_a zeros(1, a(i))];
 	end
 end
 
 for i=1:length(b)
-	if rem(i, 2) == 1 % odd .:. crista
+	if rem(i, 2) == 1 % odd .:. high peak
 		x_b = [x_b ones(1, b(i))];
-	else
+	else % even .:. low peak
 		x_b = [x_b zeros(1, b(i))];
 	end
 end
 
 for i=1:length(c)
-	if rem(i, 2) == 1 % odd .:. crista
+	if rem(i, 2) == 1 % odd .:. high peak
 		x_c = [x_c ones(1, c(i))];
-	else
+	else % even .:. low peak
 		x_c = [x_c zeros(1, c(i))];
 	end
 end
 
 for i=1:length(d)
-	if rem(i, 2) == 1 % odd .:. crista
+	if rem(i, 2) == 1 % odd .:. high peak
 		x_d = [x_d ones(1, d(i))];
-	else
+	else % even .:. low peak
 		x_d = [x_d zeros(1, d(i))];
 	end
 end
 
+% plot' em all
 myend=23000;
 figure;
 subplot(4,1,1);
@@ -105,4 +77,5 @@ subplot(4,1,4);
 plot(x_d, 'k', 'LineWidth', 5);
 xlim([0 myend]); set(gca, 'XTick', [0:2000:myend]); grid on; set(gca, 'YTick', [0:1]);
 
-%%% EOF %%%
+% return the mean of the duration of all signals
+y = mean([sum(a) sum(b) sum(c) sum(d)])/1000;
