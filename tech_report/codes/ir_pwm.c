75da7a547ea6d65bf296c778fd4efd33b7e43679
Cassio Batista <cassio.batista.13@gmail.com>
2015-05-04 14:59:30 -0300

    cassio: ir_pwm funcs commented

--- tech_report/codes/ir_pwm.c
+++ tech_report/codes/ir_pwm.c
@@ -13,67 +13,79 @@
  * symbols (bits) of a complete RC-6 code word takes XXXX ms to transmit. The
  * code word is repeated every XXXX ms (4096 / 36 kHz) as long as a key 
  * remains pressed. 
+
+ * RC -> IR sensor -> Rx -> decode -> Tx -> IR Led -> TV
+
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <unistd.h>
+//#include <unistd.h>
 #include <string.h>
 
-char *decode(short int *dur, uint8_t size);
+/* Converts from duration array to bitstream */
+char *decode(uint16_t *dur, uint8_t size);
+
+/* Error handling */
 void die(char *msg, uint8_t code);
+
+/*Tx: Transmit info to IR Led (to TV) */
 void send(char *stream);
+
+/* Rx: Receive info from Photosendor TSOP18xx (from RC) */
 void receive(int gpio);
 
 int main() {
+	uint16_t x = 960; //eXtra: padding to fill duration (see func receive())
+
 	// CANAL MAIS 
-	unsigned short int a[] = { 
+	uint16_t a[] = { 
 			42864, 2500, 940, 340, 960, 320, 520, 320, 540, 1180, 1400, 320,
 			540, 320, 520, 340, 520, 320, 540, 320, 540, 320, 520, 320, 540,
-			320, 520, 760, 960, 340, 520, 760, 520, 320, 960, 320, 540, 320, 960,
+			320, 520, 760, 960, 340, 520, 760, 520, 320, 960, 320, 540, 320, x,
 			15724, 2500, 940, 320, 960, 340, 520, 320, 540, 1180, 1400, 320,
 			540, 320, 520, 340, 520, 320, 520, 340, 520, 320, 540, 320, 540,
 			320, 520, 760, 960, 320, 540, 760, 520, 320, 960, 320, 540, 320
 	};
 
 	// CANAL MENOS
-	unsigned short int b[] = { 
+	uint16_t b[] = { 
 			39128, 2500, 940, 340, 960, 320, 520, 340, 520, 1180, 1400, 320,
 			540, 320, 540, 320, 520, 320, 540, 320, 520, 340, 520, 320, 540,
-			320, 540, 740, 960, 340, 520, 760, 520, 320, 960, 760, 960, 16164, 2480,
+			320, 540, 740, 960, 340, 520, 760, 520, 320, 960, 760, x, 16164, 2480,
 			960, 320, 960, 320, 540, 320, 520, 1200, 1380, 340, 520, 320, 540,
 			320, 540, 320, 520, 340, 520, 320, 520, 340, 520, 320, 540, 760,
 			960, 320, 520, 760, 520, 340, 940, 760 
 	};
  
 	// LIGAR / DESLIGAR
-	unsigned short int c[] = { 
+	uint16_t c[] = { 
 			34160, 2500, 960, 320, 960, 320, 520, 340, 520, 1200, 1380, 340,
 			520, 320, 540, 320, 520, 340, 520, 320, 540, 320, 520, 340, 520,
 			320, 540, 320, 520, 340, 520, 320, 540, 760, 520, 320, 960, 320,
-			540, 320, 960, 15704, 2480, 960, 320, 960, 340, 520, 320, 520, 1200,
+			540, 320, x, 15704, 2480, 960, 320, 960, 340, 520, 320, 520, 1200,
 			1400, 320, 520, 340, 520, 320, 540, 320, 520, 340, 520, 320, 540,
 			320, 540, 320, 520, 320, 540, 320, 520, 340, 520, 760, 520, 340,
 			940, 320, 540, 320
 	};
 
 	// VOLUME MAIS
-	unsigned short int d[] = { 
+	uint16_t d[] = { 
 			46568, 2500, 960, 320, 940, 340, 520, 320, 540, 1200, 1380, 320,
 			540, 320, 540, 320, 520, 320, 540, 320, 520, 340, 520, 320, 540,
 			320, 520, 340, 520, 320, 540 , 760, 960, 320, 520, 340, 520, 320,
-			540, 320, 960, 15704, 2500, 940, 320, 960, 320, 540, 320, 540, 1180,
+			540, 320, x, 15704, 2500, 940, 320, 960, 320, 540, 320, 540, 1180,
 			1400, 320, 520, 340, 520, 320, 540, 320, 540, 320, 520, 320, 540,
 			320, 520, 340, 520, 320, 540, 320, 520, 760, 960, 320, 540, 320,
 			540, 320, 520, 340
 	};
 
 	// VOLUME MENOS
-	unsigned short int e[] = { 
+	uint16_t e[] = { 
 			6124, 2480, 920, 360, 960, 320, 520, 340, 520, 1200, 1380, 320, 540,
 			320, 540, 320, 520, 340, 520, 320, 520, 340, 520, 320, 540, 320,
-			540, 320, 520, 340, 520, 760, 960, 320, 520, 340, 520, 760, 960, 16144,
+			540, 320, 520, 340, 520, 760, 960, 320, 520, 340, 520, 760, x, 16144,
 			2480, 940, 340, 960, 320, 540, 320, 520, 1200, 1400, 320, 520, 320,
 			540, 320, 540, 320, 520, 340, 520, 320, 520, 340, 520, 320, 540,
 			320, 540, 320, 520, 760, 960, 320, 540, 320, 520, 760
@@ -86,18 +98,18 @@ int main() {
 	printf("V- : %s\n", decode(e, sizeof(e)/sizeof(short)));
 
 	return EXIT_SUCCESS;
-}
+}//close main
 
 /* receives an duration vector and outputs a bit stream */
-char *decode(short int *dur, uint8_t size)
+char *decode(uint16_t *dur, uint8_t size)
 {
 	uint8_t i, j;
 	char *bitstream = (char *) malloc((22+1) * sizeof(char));
 
-	float len = 0;
 	uint8_t stay = 1; 
 	uint8_t high = 1;
 
+	//float len = 0;
 	//for(i=1,j=0; i<size; i++) {
 	//	if (dur[i] > 4000)
 	//		break;
@@ -108,9 +120,9 @@ char *decode(short int *dur, uint8_t size)
 	for(i=1,j=0; i<size; i++) {
 		/* check AGC bit */
 		if((dur[i]+dur[i+1])>=3400) {
-			if(dur[i]>3000){ 
+			if(dur[i]>3000)
 				goto termina;
-			}
+
 			if(j==0) {
 				bitstream[j++] = '1';
 				stay = 0;
@@ -133,46 +145,42 @@ char *decode(short int *dur, uint8_t size)
 			goto termina;
 		}
 
-		if(strlen(bitstream)==22) 
-			break;
-
+		/* check common bits */
 		if((dur[i]+dur[i+1])>=(360+560)) {
-			if((high==1) && (dur[i+1]>(560))) {
-				high = 0;
-				stay = 1;
+			if(high) {
 				bitstream[j++] = '1';
-			}
-
-			else if((high==1) && (dur[i+1]<=(560))) {
-				stay = 0;
-				high = 1;
-				bitstream[j++] = '1';
-			}
-
-			else if((high==0) && (dur[i+1]>(360))) {
-				high = 1;
-				stay = 1;
+				if(dur[i+1]>560) {
+					high = 0;
+					stay = 1;
+				} else {
+					high = 1;
+					stay = 0;
+				}
+			} else {
 				bitstream[j++] = '0';
-			}
-
-			else if((high==0) && (dur[i+1]<=(360))) {
-				high = 0;
-				stay = 0;
-				bitstream[j++] = '0';
+				if(dur[i+1]>360) {
+					high = 1;
+					stay = 1;
+				} else {
+					high = 0;
+					stay = 0;
+				}
 			}
 		} else {
 			if(high) {
-				stay = 0;
 				high = 1;
+				stay = 0;
 				bitstream[j++] = '1';
 			} else {
-				stay = 0;
 				high = 0;
+				stay = 0;
 				bitstream[j++] = '0';
 			}
 		}
 
 		termina:
+		if(strlen(bitstream)==22) 
+			break;
 		if(stay==0) 
 			i++;
 	}
@@ -185,6 +193,7 @@ void die(char *msg, uint8_t code) {
 	exit(code);
 }
 
+/* Gabriel: fprintf e fopen for GPIO*/
 /* send IR info to TV */
 void send(char *stream)
 {
@@ -216,9 +225,9 @@ void send(char *stream)
 	for(i=1; i<strlen(stream); i++) {
 		/* Toggle */
 		if(i==5) 
-			pulses=32;
+			pulses=2*16;
 		else 
-			pulses=16;
+			pulses=1*16;
 
 		if(stream[i]=='1') {
 			for(j=0; j<pulses; j++) {
@@ -246,10 +255,11 @@ void send(char *stream)
 }//close send
 
 /* thiago: converts from arduino to c */
+/* Receive IR info from Remote Control */
 void receive(int gpio) 
 {
 /* t = 16 * (1/36) */
-/* duration = sum([6*t 2*t 2*t 2*t 20*t 20*t])*/
+/* duration_rc6 = sum([6*t 2*t 2*t 2*t 20*t 20*t]) */
 /* sum(vector_of_durations) = 23111 */
 /* thiago */
 /* thiago */
