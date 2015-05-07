88c01b1cac9b9d4bc5900b867ae5005bae4046f2
gaburiero <gaburiero.c@gmail.com>
2015-05-07 19:27:13 -0300

    adicionando bbbgpio lib

---
+++ gpio/lib/bbbgpio.h
@@ -0,0 +1,29 @@
+/*
+ *Author: gaburiero
+ *Based on Derek Malloy's software
+ *
+*/ 
+
+#ifndef BBBGPIO_H_
+#define BBBGPIO_H_
+
+#define MAX 64
+#define GPIO_LOCATION "/sys/class/gpio"
+
+enum PIN_DIR{
+    INPUT=0;
+    OUTPUT=1;
+};
+
+enum PI_VALUE{
+    LOW=0;
+    HIGH=1;
+};
+
+void gpioExport(unsigned int gpio);
+void gpioUnexport(unsigned int gpio);
+void gpioSetDirection(unsigned int gpio, PIN_DIR flag);
+void gpioSetValue(unsigned int gpio, PIN_VALUE value);
+
+
+#endif
